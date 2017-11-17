8d94f02aad51c47b35384d1ff9e88c6ae04ce026
Quang Kiet TRAN <quangkiet@vpn4-26.f6.fr>
2017-11-17 18:20:26 +0700

    fix(layout): change layout to fullscreen

--- src/assets/js/Homepage/Homepage.js
+++ src/assets/js/Homepage/Homepage.js
@@ -8,6 +8,7 @@ export default class Homepage {
     this.$prodHours = $('.js-prod-hours');
     this.$prodMins = $('.js-prod-mins');
     this.$prodSecs = $('.js-prod-secs');
+    this.$prodCarousel = $('.js-prod-carousel');
     this.$window = globals.$window;
   }
 
@@ -19,21 +20,62 @@ export default class Homepage {
     });
   }
 
+  productCarousel() {
+    this.$prodCarousel.owlCarousel({
+      nav: true,
+      margin: 10,
+      loop: true,
+      navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+      responsive: {
+        1200: {
+          items: 4,
+        },
+        1024: {
+          items: 3,
+        },
+        768: {
+          items: 2,
+        },
+        480: {
+          items: 2,
+        },
+        320: {
+          items: 2,
+        },
+      },
+    });
+  }
+
   productCountDown() {
-    const countDownDate = new Date('Nov 16, 2017 16:50:25').getTime();
-    setInterval(() => {
+    const countDownDate = new Date('Dec 15, 2017 11:30:00').getTime();
+    const countDown = setInterval(() => {
       const now = new Date().getTime();
       const distance = countDownDate - now;
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      if (days < 10) {
+        days = `0${days}`;
+      }
+      let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      if (hours < 10) {
+        hours = `0${hours}`;
+      }
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      if (minutes < 10) {
+        minutes = `0${minutes}`;
+      }
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      if (seconds < 10) {
+        seconds = `0${seconds}`;
+      }
 
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-      this.$prodDays.html(days);
-      this.$prodHours.html(hours);
-      this.$prodMins.html(minutes);
-      this.$prodSecs.html(seconds);
+      if (distance >= 0) {
+        this.$prodDays.html(days);
+        this.$prodHours.html(hours);
+        this.$prodMins.html(minutes);
+        this.$prodSecs.html(seconds);
+      } else {
+        clearInterval(countDown);
+      }
     });
   }
 
@@ -42,6 +84,7 @@ export default class Homepage {
 
     homepage.homeCarousel();
     homepage.productCountDown();
+    homepage.productCarousel();
 
     return homepage;
   }
